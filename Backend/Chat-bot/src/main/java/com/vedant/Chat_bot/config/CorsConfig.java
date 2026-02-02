23928f438def567d804514ffe2769ba6110d8ebc
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-02-02 13:47:30 +0530

    Update CorsConfig.java

--- Backend/Chat-bot/src/main/java/com/vedant/Chat_bot/config/CorsConfig.java
+++ Backend/Chat-bot/src/main/java/com/vedant/Chat_bot/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://summar-ai-kohl.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://summar-ai-kohl.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
