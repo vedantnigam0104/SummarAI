ca1c6a9087057965cbc7858d346ce77a20de6737
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-02-04 17:11:01 +0530

    Update ChatBotApplication.java

--- Backend/Chat-bot/src/main/java/com/vedant/Chat_bot/ChatBotApplication.java
+++ Backend/Chat-bot/src/main/java/com/vedant/Chat_bot/ChatBotApplication.java
@@ -2,15 +2,8 @@ package com.vedant.Chat_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class ChatBotApplication {
 
 	public static void main(String[] args) {
