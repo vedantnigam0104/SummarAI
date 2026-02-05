01522e319d4c35e30a548a3006f16bf9647c6a9c
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-02-05 17:31:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./context/UserContext";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
       <App />
+      {/* 🔔 Toast container */}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
     </UserProvider>
   </React.StrictMode>
 );
